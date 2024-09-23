db05c2ca2155830e5e5a6d64eb95a67bed8a0ddf
BALAJI934428 <142514847+BALAJI934428@users.noreply.github.com>
2024-09-23 13:54:57 +0530

    commit for deploy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,11 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api",
+    "/api", // Proxy requests starting with `/api`
     createProxyMiddleware({
-      target: "https://ecommerce-backend-3phr.onrender.com",
+      target: "https://ecommerce-backend-3phr.onrender.com", // Backend API URL
       changeOrigin: true,
+      secure: false, // Use `false` if you're working with self-signed SSL certificates
     })
   );
 };
